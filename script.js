416d8b0b9a23984f9dc1d6e4abfc417e4617a095
blessNcode <blessingoyor2008@gmail.com>
2025-06-30 23:31:58 -0400

    start on projects

--- script.js
+++ script.js
@@ -8,3 +8,51 @@ function hideSidebar(){
  const sidebar = document.querySelector (".sidebar");
  sidebar.style.display= "none";
 }
+
+let list = document.querySelector(".slider .list");
+let items = document.querySelectorAll(".slider .list .slider-item");
+let dots = document.querySelectorAll(".slider .dots li");
+let prev = document.getElementById("prev");
+let next = document.getElementById("next");
+
+let active = 0;
+let lengthItems = items.length - 1;
+
+next.onclick = function() {
+    if (active + 1 > lengthItems) {
+        active = 0;
+    } else {
+        active = active + 1;
+    };
+    reloadSlider ();
+};
+
+prev.onclick = function() {
+    if ( active - 1 < 0 ) {
+        active = lengthItems;
+    } else {
+        active = active - 1;
+    }
+    reloadSlider();
+}
+
+let refreshSlider = setInterval (() => {next.click()}, 7000);
+
+function reloadSlider() {
+    let checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + 'px';
+
+    let lastActiveDot = document.querySelector ('.slider .dots li.active')
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+    clearInterval(refreshSlider);
+    let refreshSlider = setInterval (() => {next.click()}, 7000);
+};
+
+dots.forEach ((list, key) => {
+    list.addEventListener('click', function() {
+        active = key;
+        reloadSlider();
+    })
+})
+
